3f5e8c1cc17bbaae78e939010b6e1c79dfe27e77
iftiaz <iftiaz.ahmed@yahoo.com>
2024-10-21 00:10:28 -0400

    frontend-navbar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,15 @@
+import logo from './logo.png'
+import search from './search.png'
+import profile from './profile_icon.png'
+import cart from './cart.png'
+import menu_icon from './menu_icon.png'
+import left_arrow from './left_arrow.png'
+
+export const assets = {
+    logo,
+    search,
+    profile,
+    cart,
+    menu_icon,
+    left_arrow
+}
